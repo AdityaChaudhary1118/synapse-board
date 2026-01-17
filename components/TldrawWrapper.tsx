c7d2499c5e0605abcb873a4c4a84e2d39bc13a93
AdityaChaudhary1118 <aka2005711@gmailcom>
2026-01-17 21:39:43 +0530

    Test: Remove button to isolate crash

--- components/TldrawWrapper.tsx
+++ components/TldrawWrapper.tsx
@@ -2,14 +2,18 @@
 
 import { Tldraw } from "tldraw";
 import "tldraw/tldraw.css";
-import { MakeRealButton } from "./MakeRealButton"; 
+// import { MakeRealButton } from "./MakeRealButton"; <--- Commented out for safety
 
 export default function TldrawWrapper() {
   return (
-    <div className="w-screen h-screen relative">
-      {/* persistenceKey is omitted to prevent "corrupt data" crashes */}
-      <Tldraw>
-        <MakeRealButton />
+    <div className="w-screen h-screen relative" style={{ position: 'fixed', inset: 0 }}>
+      <Tldraw 
+        persistenceKey={null}
+        onMount={(editor) => {
+            console.log("Whiteboard Mounted!", editor);
+        }}
+      >
+        {/* <MakeRealButton />  <--- Disabled to test if this is the cause */}
       </Tldraw>
     </div>
   );
