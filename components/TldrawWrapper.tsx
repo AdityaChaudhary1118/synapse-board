2ce9df7c06181b89359c5842154fc80abbef161c
AdityaChaudhary1118 <aka2005711@gmailcom>
2026-01-17 21:53:07 +0530

    Test: Empty board only

--- components/TldrawWrapper.tsx
+++ components/TldrawWrapper.tsx
@@ -2,28 +2,11 @@
 
 import { Tldraw } from "tldraw";
 import "tldraw/tldraw.css";
-import { MakeRealButton } from "./MakeRealButton";
-// import { useYjsStore } from "@/app/useYjsStore"; <--- Commented out to stop the crash
 
 export default function TldrawWrapper() {
-  // const store = useYjsStore({ roomId: "synapse-room-1" }); <--- Commented out
-
   return (
-    <div 
-      className="relative" 
-      style={{ 
-        position: 'fixed', 
-        inset: 0, 
-        width: '100vw', 
-        height: '100vh',
-        zIndex: 50,
-        backgroundColor: '#fff' // White background to ensure visibility
-      }}
-    >
-      {/* store={store} is REMOVED. This runs Tldraw in "Offline Mode" */}
-      <Tldraw persistenceKey="testing-room">
-        <MakeRealButton />
-      </Tldraw>
+    <div style={{ position: 'fixed', inset: 0, width: '100vw', height: '100vh' }}>
+      <Tldraw persistenceKey="clean-test-room" />
     </div>
   );
 }
