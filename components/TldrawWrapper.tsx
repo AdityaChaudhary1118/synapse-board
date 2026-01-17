a9307e6eb70f54590f5ccdf7eed13a7857c8ed42
AdityaChaudhary1118 <aka2005711@gmailcom>
2026-01-17 21:50:29 +0530

    Fix: Update persistence key

--- components/TldrawWrapper.tsx
+++ components/TldrawWrapper.tsx
@@ -2,14 +2,27 @@
 
 import { Tldraw } from "tldraw";
 import "tldraw/tldraw.css";
-import { MakeRealButton } from "./MakeRealButton"; // Uncomment this
+import { MakeRealButton } from "./MakeRealButton";
+// import { useYjsStore } from "@/app/useYjsStore"; <--- Commented out to stop the crash
 
 export default function TldrawWrapper() {
+  // const store = useYjsStore({ roomId: "synapse-room-1" }); <--- Commented out
+
   return (
-    <div className="w-screen h-screen relative" style={{ position: 'fixed', inset: 0 }}>
-      {/* persistenceKey={null} prevents "corrupt data" crashes */}
-      <Tldraw persistenceKey={null}>
-        <MakeRealButton /> {/* Uncomment this */}
+    <div 
+      className="relative" 
+      style={{ 
+        position: 'fixed', 
+        inset: 0, 
+        width: '100vw', 
+        height: '100vh',
+        zIndex: 50,
+        backgroundColor: '#fff' // White background to ensure visibility
+      }}
+    >
+      {/* store={store} is REMOVED. This runs Tldraw in "Offline Mode" */}
+      <Tldraw persistenceKey="testing-room">
+        <MakeRealButton />
       </Tldraw>
     </div>
   );
